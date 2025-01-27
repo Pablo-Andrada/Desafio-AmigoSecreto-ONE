c074bcca8969c4445a616f6dc012405c75be4204
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-01-27 00:41:08 -0300

    Terminando en app.js

--- app.js
+++ app.js
@@ -37,3 +37,22 @@ function mostrarListaAmigos() {
     listaAmigosElement.appendChild(li);
   });
 }
+// Función para sortear un amigo
+function sortearAmigo() {
+    // Verificar si el array amigos no está vacío
+    if (amigos.length === 0) {
+      alert("No hay amigos para sortear.");
+      return;
+    }
+  
+    // Generar un índice aleatorio entre 0 y la longitud del array - 1
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  
+    // Obtener el nombre sorteado
+    const amigoSorteado = amigos[indiceAleatorio];
+  
+    // Mostrar el resultado en el elemento con id "resultado"
+    const resultadoElemento = document.getElementById("resultado");
+    resultadoElemento.innerHTML = `<li><strong>El amigo secreto es:</strong> ${amigoSorteado}</li>`;
+  }
+  
